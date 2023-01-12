fc99f5863e2045ae728b1ced9c2e3a8e9ca576a7
keshab <keshabjoshi998@gmail.com>
2023-01-12 14:17:54 +0545

    "backend completee"

--- backend/src/main/java/com/makalu/hrm/controller/AttendanceController.java
+++ backend/src/main/java/com/makalu/hrm/controller/AttendanceController.java
@@ -1,11 +1,14 @@
 package com.makalu.hrm.controller;
 
 
+
+import com.makalu.hrm.model.AttendanceDto;
 import com.makalu.hrm.model.RestResponseDto;
 import com.makalu.hrm.repository.AttendanceRepository;
 import com.makalu.hrm.repository.UserRepository;
 import com.makalu.hrm.service.impl.AttendanceServiceImp;
-import com.makalu.hrm.utils.AuthenticationUtils;
+
+import com.makalu.hrm.utils.IPUtils;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 
 @Controller
@@ -20,6 +25,8 @@ import org.springframework.web.bind.annotation.*;
 
 public class AttendanceController {
 private final AttendanceServiceImp attendanceServiceImp;
+private  final AttendanceRepository attendanceRepository;
+private  final UserRepository userRepository;
 
 
 
@@ -27,21 +34,46 @@ private final AttendanceServiceImp attendanceServiceImp;
 
 
 
-    @GetMapping("/attendance")
 
-    public ResponseEntity<RestResponseDto> hit() {
-         com.book.model.AttendanceDto attendanceDto =new com.book.model.AttendanceDto();
-      attendanceDto.setPunchInIp("1.0.0.0.1");
+    @GetMapping("/attendance/punchin")
+
+    public ResponseEntity<RestResponseDto> punchIn(HttpServletRequest request) {
+        AttendanceDto attendanceDto =new AttendanceDto();
+
+         //getting client ip
+         String clientIp=  IPUtils.getClientIp(request);
 
 
+        if( IPUtils.ismakaluNetwork(clientIp)){
+      attendanceDto.setPunchInIp(clientIp);
+            return ResponseEntity.ok().body(attendanceServiceImp.save(attendanceDto));
 
-
-        return ResponseEntity.ok().body(attendanceServiceImp.save(attendanceDto));
+        }
+     return  ResponseEntity.ok().body(RestResponseDto.INSTANCE().message("yo cannot login outside of office"));
 
 
     }
 
 
+//PersistentAttendanceEntity entity=new PersistentAttendanceEntity();
+//entity.setPunchInIp("1.0.0.9");
+//entity.setUser(userRepository.findByUsername("dhirajbadu50@gmail.com").get());
+//
+//    return ResponseEntity.ok().body(attendanceRepository.save(entity));
+
+
+
+    @GetMapping("/attendance/punchout")
+    public ResponseEntity<RestResponseDto> punchout(HttpServletRequest request){
+
+AttendanceDto attendanceDto=new AttendanceDto();
+attendanceDto.setPunchOutDate(new Date());
+attendanceDto.setPunchOutIp(IPUtils.getClientIp(request));
+
+return ResponseEntity.ok().body(attendanceServiceImp.update(attendanceDto));
+
+
+    }
 
 
 
