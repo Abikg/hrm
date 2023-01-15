42157dcbf2dccce8b213186b9d9644e452abbee1
dhiraj <dhirajbadu50@gmail.com>
2023-01-15 10:17:36 +0545

    merge conflict resolved

--- backend/src/main/java/com/makalu/hrm/service/UserService.java
+++ backend/src/main/java/com/makalu/hrm/service/UserService.java
@@ -3,7 +3,6 @@ package com.makalu.hrm.service;
 import com.makalu.hrm.domain.PersistentUserEntity;
 import com.makalu.hrm.model.RestResponseDto;
 import javax.validation.constraints.NotNull;
-import java.util.UUID;
 
 public interface UserService {
 
@@ -11,6 +10,5 @@ public interface UserService {
 
     RestResponseDto createEmployeeUser(@NotNull String email);
 
-    RestResponseDto updateEmployeeUser(String email, UUID userId);
 }
 
