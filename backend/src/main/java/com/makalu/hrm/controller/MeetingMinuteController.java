8b035111d3709429b1141d5e050a212280bd0704
keshab <keshabjoshi998@gmail.com>
2023-01-17 20:48:31 +0545

    ("meeting minute")

--- backend/src/main/java/com/makalu/hrm/controller/MeetingMinuteController.java
+++ backend/src/main/java/com/makalu/hrm/controller/MeetingMinuteController.java
@@ -2,27 +2,32 @@ package com.makalu.hrm.controller;
 
 import com.makalu.hrm.constant.ParameterConstant;
 import com.makalu.hrm.enumconstant.MeetingType;
+import com.makalu.hrm.enumconstant.UserType;
 import com.makalu.hrm.model.MeetingMinutesDto;
 import com.makalu.hrm.model.RestResponseDto;
 import com.makalu.hrm.service.MeetingMinuteService;
 
+import com.makalu.hrm.utils.AuthenticationUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 
 @RequestMapping("/meetingMinutes")
 @Controller
 @RequiredArgsConstructor
-public class EmployeeMeetingMinuteController {
+public class MeetingMinuteController {
 private final MeetingMinuteService meetingMinuteMinuteService;
 
 
-@GetMapping("/employeeForm/{meetingtype}")
+    @GetMapping("/employeeForm/{meetingtype}")
         public String showform(@PathVariable MeetingType meetingtype,ModelMap map){
     if(MeetingType.EMPLOYEE.name().equals(meetingtype.name())){
     map.addAttribute(ParameterConstant.MEETING_TYPE.toUpperCase(),meetingtype.name().toUpperCase());
@@ -43,7 +48,7 @@ private final MeetingMinuteService meetingMinuteMinuteService;
 
 
 
-
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
     @GetMapping("/bod/list")
     public String List(ModelMap map){
         map.addAttribute(ParameterConstant.MEETING_LIST,meetingMinuteMinuteService.findAll(MeetingType.BOD));
@@ -66,18 +71,32 @@ private final MeetingMinuteService meetingMinuteMinuteService;
 
     @PostMapping("/save")
     @ResponseBody
-   public ResponseEntity<RestResponseDto> create(MeetingMinutesDto meetingDto){
+   public ResponseEntity<RestResponseDto> create(MeetingMinutesDto meetingDto) {
+        if (meetingDto.getMeetingType().name().toUpperCase().equals(MeetingType.BOD.name().toUpperCase())) {
+            if (AuthenticationUtils.getCurrentUser().getUserType().equals(UserType.SUPER_ADMIN.name())) {
+                return ResponseEntity.ok(meetingMinuteMinuteService.save(meetingDto));
+            }
+        }
+            return ResponseEntity.ok(meetingMinuteMinuteService.save(meetingDto));
+        }
 
-        return ResponseEntity.ok(meetingMinuteMinuteService.save(meetingDto));
-    }
+
 
 
     @GetMapping("/showMinute/{id}")
     public String show(@PathVariable UUID id,ModelMap map){
 
     map.addAttribute("Minute",meetingMinuteMinuteService.findById(id).getDetail());
+     return "meetingMinute/minute";
+    }
 
-    return "meetingMinute/minute";
+    @GetMapping("/getAuthorities")
+    public ResponseEntity<UserType> showAuthorities(){
+
+        List<String> stringList=new ArrayList<>();
+       // stringList=
+        UserType u=AuthenticationUtils.getCurrentUser().getUserType();
+        return ResponseEntity.ok().body(u);
     }
 }
 
