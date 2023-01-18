c883addb1bc716ef5a10900d32a749ff53f0b8c3
keshab <keshabjoshi998@gmail.com>
2023-01-18 21:34:32 +0545

    ("backend complete")

--- backend/src/main/java/com/makalu/hrm/controller/MeetingMinuteController.java
+++ backend/src/main/java/com/makalu/hrm/controller/MeetingMinuteController.java
@@ -29,22 +29,7 @@ private final MeetingMinuteService meetingMinuteMinuteService;
 private  final UserService userService;
 
 
-    @GetMapping("/employeeForm/{meetingtype}")
-        public String showform(@PathVariable MeetingType meetingtype,ModelMap map){
-    if(MeetingType.EMPLOYEE.name().equals(meetingtype.name())){
 
-    map.addAttribute(ParameterConstant.MEETING_TYPE.toUpperCase(),meetingtype.name().toUpperCase());
-    map.addAttribute("attendedBy",userService.findAllByUserType(UserType.EMPLOYEE));
-
-    }else if(MeetingType.BOD.name().equals(meetingtype.name())){
-
-
-        map.addAttribute(ParameterConstant.MEETING_TYPE.toUpperCase(),meetingtype.name().toUpperCase());
-        map.addAttribute("attendedBy",userService.findAllByUserType(UserType.SUPER_ADMIN));
-    }
-
-   return "meetingMinute/meetingForm";
-}
 
     @GetMapping("/employees/list")
     public String showEmployeesMeeting(ModelMap map){
@@ -54,7 +39,6 @@ private  final UserService userService;
 
         return "meetingMinute/list";
     }
-
 
 
     @PreAuthorize("hasRole('SUPER_ADMIN')")
@@ -69,18 +53,52 @@ private  final UserService userService;
 
 
 
-    @PostMapping("/save")
 
-   public String create(MeetingMinutesDto meetingDto, @RequestParam("attendedByy") List<UUID> attendById, ModelMap map) {
-        if (meetingDto.getMeetingType().name().toUpperCase().equals(MeetingType.BOD.name().toUpperCase())) {
-            if (AuthenticationUtils.getCurrentUser().getUserType().equals(UserType.SUPER_ADMIN)) {
+    @GetMapping("/employeeForm/{meetingtype}")
+    public String showform(@PathVariable MeetingType meetingtype,ModelMap map){
+        if(MeetingType.EMPLOYEE.name().equals(meetingtype.name())){
 
-    map.addAttribute(ParameterConstant.RESPONSE,meetingMinuteMinuteService.save(meetingDto,attendById));
+            map.addAttribute(ParameterConstant.MEETING_TYPE.toUpperCase(),meetingtype.name().toUpperCase());
+            map.addAttribute("attendedBy",userService.findAllByUserType(UserType.EMPLOYEE));
+            map.addAttribute("MEETINGTYPEURL","/meetingMinutes/employee/save");
+
+        }else if(MeetingType.BOD.name().equals(meetingtype.name())){
 
 
-               return "meetingMinute/meetingForm";
-            }
+            map.addAttribute(ParameterConstant.MEETING_TYPE.toUpperCase(),meetingtype.name().toUpperCase());
+            map.addAttribute("attendedBy",userService.findAllByUserType(UserType.SUPER_ADMIN));
+            map.addAttribute("MEETINGTYPEURL","/meetingMinutes/bod/save");
         }
+
+        return "meetingMinute/meetingForm";
+    }
+
+
+
+
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
+    @PostMapping("/bod/save")
+
+    public String save(MeetingMinutesDto meetingDto, @RequestParam("attendedByy") List<UUID> attendById, ModelMap map) {
+
+
+           map.addAttribute(ParameterConstant.RESPONSE,meetingMinuteMinuteService.save(meetingDto,attendById));
+
+
+                return "meetingMinute/meetingForm";
+
+
+
+    }
+
+
+
+
+
+
+    @PostMapping("/employee/save")
+    public String create(MeetingMinutesDto meetingDto, @RequestParam("attendedByy") List<UUID> attendById, ModelMap map) {
+
         map.addAttribute(ParameterConstant.RESPONSE,meetingMinuteMinuteService.save(meetingDto,attendById));
 
         return "meetingMinute/meetingForm";
