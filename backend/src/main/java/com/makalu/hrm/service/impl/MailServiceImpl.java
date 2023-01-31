f5da4323c65a33e08fa2f10f358fdfd3411c3845
dhiraj <dhirajbadu50@gmail.com>
2023-01-31 11:13:47 +0545

    updated

--- backend/src/main/java/com/makalu/hrm/service/impl/MailServiceImpl.java
+++ backend/src/main/java/com/makalu/hrm/service/impl/MailServiceImpl.java
@@ -4,11 +4,13 @@ import com.makalu.hrm.model.RestResponseDto;
 import com.makalu.hrm.service.MailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
 
 @Service
 @Slf4j
@@ -32,4 +34,22 @@ public class MailServiceImpl implements MailService {
             return RestResponseDto.INSTANCE().internalServerError();
         }
     }
+
+    @Override
+    public RestResponseDto sendHtmMail(String to, String subject, String body) {
+        try {
+            MimeMessage mail = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            javaMailSender.send(mail);
+            return RestResponseDto.INSTANCE().success();
+        } catch (Exception ex) {
+            log.error("Error sending mail", ex);
+            return RestResponseDto.INSTANCE().internalServerError();
+        }
+    }
 }
+
+
--- backend/src/main/java/com/makalu/hrm/service/impl/MailTemplateParserImpl.java
+++ backend/src/main/java/com/makalu/hrm/service/impl/MailTemplateParserImpl.java
@@ -4,6 +4,7 @@ import com.makalu.hrm.service.MailTemplateParser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
@@ -14,12 +15,12 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class MailTemplateParserImpl implements MailTemplateParser {
 
-    private final SpringTemplateEngine springTemplateEngine;
+    private final TemplateEngine templateEngine;
 
     @Override
     public String sendPasswordTemplate(Map<String, Object> context){
         Context ctx = new Context();
         context.forEach(ctx::setVariable);
-        return springTemplateEngine.process("/mail/password.html",ctx);
+        return templateEngine.process("/mail/password.html",ctx);
     }
 }
